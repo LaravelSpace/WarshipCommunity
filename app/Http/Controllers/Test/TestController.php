b9de35a1262dd67068ffa23b0f3f1f957ba1e644
KelipuTe <xhy_1365@sina.com>
2019-08-11 18:13:20 +0800

    readme 20190811

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Test;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use ReflectionClass;
 
 class TestController extends Controller
 {
--- app/NotesManuscript/draft/mysql-piece.md
+++
@@ -1,5 +0,0 @@
-# Mysql 碎片知识点
-
-## Mysql 数据类型
-
-`tinyint(1)` 和 `tinyint(4)` 都可以存储 127。
