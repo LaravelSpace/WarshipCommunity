22ca04a437dd37ac2b29d9ff58d8651924a44123
KelipuTe <xhy_1365@sina.com>
2019-11-27 20:34:17 +0800

    readme 20191127

--- resources/views/community/article/article-list.blade.php
+++ resources/views/community/article/article-list.blade.php
@@ -13,23 +13,9 @@
                 </div>
             </li>
         </ul>
-        <nav aria-label="article paginate navigation">
-            <ul class="pagination pagination-lg justify-content-center">
-                <li class="page-item" :class="prevPage">
-                    <a class="page-link" href="#" :href="prevPageUrl" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                <li class="page-item" v-for="page in paginate.page_list"><a class="page-link" href="#" :href="pageUrl(page)">@{{ page }}</a></li>
-                <li class="page-item" :class="nextPage">
-                    <a class="page-link" href="#" :href="nextPageUrl" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <div id="vue-paginate" v-if="vifPaginateShow">
+            <vue-paginate v-bind:parent_paginate="paginate"></vue-paginate>
+        </div>
     </div>
 </template>
 <script>
@@ -48,53 +34,89 @@
         methods: {
             getArticleList: function (page) {
                 let thisVue = this;
-                let url = URI_API.article;
+                let uri = URI_API.article;
                 if (page !== null && page !== "" && page > 0) {
-                    url += '?page=' + page;
+                    uri += '?page=' + page;
                 }
-                axios.get(url).then(function (response) {
+                axios.get(uri).then(function (response) {
                     thisVue.articleList = response.data.data.article_list;
                     if (thisVue.articleList.length > 0) {
                         thisVue.vifArticleShow = true;
                     }
                     thisVue.paginate = response.data.data.paginate;
-                    if (thisVue.paginate.length > 0) {
+                    if (thisVue.paginate.page_list.length > 0) {
                         thisVue.vifPaginateShow = true;
                     }
                 }).catch(function (error) {
                     console.error(error.response);
                 });
-            },
+            }
+        }
+    });
+    new Vue({el: "#article-list"});
+</script>
+
+<template id="template-vue-paginate">
+    <nav aria-label="article paginate navigation">
+        <ul class="pagination pagination-lg justify-content-center">
+            <li class="page-item" :class="prevPage">
+                <a class="page-link" href="#" :href="prevPageUrl" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+            <li class="page-item" v-for="page in paginate.page_list">
+                <a class="page-link" href="#" :href="pageUrl(page)">@{{ page }}</a>
+            </li>
+            <li class="page-item" :class="nextPage">
+                <a class="page-link" href="#" :href="nextPageUrl" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+    Vue.component("vue-paginate", {
+        template: "#template-vue-paginate",
+        props: ['parent_paginate'],
+        data: function () {
+            return {
+                paginate: this.parent_paginate, vifPaginateShow: false
+            }
+        },
+        methods: {
             pageUrl: function (page) {
-                return URI_WEB.article + '?page=' + page
+                return URI_WEB.article + '?page=' + page;
             }
         },
         computed: {
             prevPage: function () {
                 if (this.paginate.prev_page === null || this.paginate.prev_page === "") {
-                    return "disabled"
+                    return "disabled";
                 }
                 return "";
             },
             prevPageUrl: function () {
                 if (this.paginate.prev_page === null || this.paginate.prev_page === "") {
-                    return URI_WEB.article + '?page=' + this.paginate.prev_page
+                    return URI_WEB.article + '?page=' + this.paginate.prev_page;
                 }
                 return "";
             },
             nextPage: function () {
                 if (this.paginate.next_page === null || this.paginate.next_page === "") {
-                    return "disabled"
+                    return "disabled";
                 }
                 return "";
             },
             nextPageUrl: function () {
                 if (this.paginate.next_page === null || this.paginate.next_page === "") {
-                    return URI_WEB.article + '?page=' + this.paginate.next_page
+                    return URI_WEB.article + '?page=' + this.paginate.next_page;
                 }
                 return "";
             }
         }
     });
-    new Vue({el: "#article-list"});
+    new Vue({el: "#vue-paginate"});
 </script>
